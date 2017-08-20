ea1be658ce5c2beacd07f4cb9d8f8f0aa9f8f288
Abhishek Gupta <ag3088@AGs-Mac.home>
2017-08-20 14:23:37 -0400

    Initial Commit

---
+++ vacationer/vacationer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vacationer
+//
+//  Created by Abhishek Gupta on 8/20/17.
+//  Copyright (c) 2017 Travelers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
